c9dec9552883a8324ffceae64d7742fe522fabca
Allana Evellyn <allanaevellynm@gmail.com>
2022-03-11 21:12:35 -0300

    :pencil: type aliases com intersection

--- logDetails.ts
+++ logDetails.ts
@@ -2,7 +2,7 @@
 // o pipe | serve pra você indicar diferentes tipos que aquela variável pode ter
 // um type alias é um tipo criado por você que pode usar os diferentes tipos que vc deseja que uma variável tenha
 
-type Uid = number | string
+type Uid = number | string;
 
 //sem type alias
 /* function logDetails(uid: number | string, item: string) {
@@ -11,11 +11,11 @@ type Uid = number | string
 
 //com type alias
 function logDetails(uid: Uid, item: string) {
-    console.log(`A product with ${uid} has a title as ${item}`)
+    console.log(`A product with ${uid} has a title as ${item}`);
 }
 
 function logInfo(uid: Uid, user: string){
-    console.log(`An user with ${uid} has a name as ${user}`)
+    console.log(`An user with ${uid} has a name as ${user}`);
 }
 
 type Platform = "Windows" | "Linux" | "Mac Os" | "ios"
@@ -26,8 +26,8 @@ function renderPlatform(platform: Platform){
 
 renderPlatform('ios')
 
-logDetails(123, 'Batata')
-logDetails("123", "Batata")
+logDetails(123, 'Batata');
+logDetails("123", "Batata");
 
-logInfo(123, "Allana")
-logInfo("123", " Allana")
+logInfo(123, "Allana");
+logInfo("123", " Allana");
---
+++ primeiro-programa-com-ts/user.ts
@@ -0,0 +1,37 @@
+// accountInfo
+// charInfo
+// PlayerInfo
+
+type AccountInfo = {
+    id: number,
+    name: string,
+    //colocar ponto de interrogações para informações opcionais, logo vc n precisa falar mais de um tipo para a mesma variavel
+    email?: string
+}
+
+const account: AccountInfo = {
+    id: 123,
+    name: 'Allana',
+    email: 'allanaevellynm@gmail.com'
+}
+
+type CharInfo = {
+    nickname: string,
+    level: number
+}
+
+const char: CharInfo = {
+    nickname: 'allanina',
+    level: 100
+}
+
+
+//criar um tipo que tem os tipos de dois objetos diferentes (intersection)
+type PlayerInfo = AccountInfo & CharInfo
+
+const player: PlayerInfo = {
+    id: 1234,
+    name: "Allana",
+    nickname: 'allana',
+    level: 100
+}
