9c06179eb359572c7407e9804a73e637d3612589
Allana Evellyn <allanaevellynm@gmail.com>
2022-03-13 16:12:40 -0300

    :pencil: classes parte 02: accessors e etc

--- primeiro-programa-com-ts/classes.js
+++ primeiro-programa-com-ts/classes.js
@@ -1,37 +1,82 @@
-"use strict";
 //classes (poo)
-class UserAccount {
-    constructor(name, age) {
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+//se eu colocasse que ele era uma classe abstrata, eu poderia apenas extender aquela classe mas não poderia fazer novos objetos a partir dela (interessante quando se quer criar classes que sejam apenas modelos)
+// abstract class UserAccount {
+var UserAccount = /** @class */ (function () {
+    function UserAccount(name, age) {
         this.name = name;
         this.age = age;
     }
     //pode se criar métodos e usá-los depois
-    logDetails() {
-        console.log(`The player ${this.name} is ${this.age} years old.`);
-    }
-}
+    UserAccount.prototype.logDetails = function () {
+        console.log("The player ".concat(this.name, " is ").concat(this.age, " years old."));
+    };
+    return UserAccount;
+}());
 //uma classe que extende de outra classe
-class CharAccount extends UserAccount {
-    constructor(name, age, nickname, level) {
+var CharAccount = /** @class */ (function (_super) {
+    __extends(CharAccount, _super);
+    function CharAccount(name, age, nickname, level) {
+        var _this = 
         //so na hora de atribuir o valor que os nomes das variaveis precisam ser iguais
         //o super() pega as propriedades da classe superior
-        super(name, age);
-        this.nickname = nickname;
-        this.level = level;
+        _super.call(this, name, age) || this;
+        _this.nickname = nickname;
+        _this.level = level;
+        return _this;
     }
-    logCharDetails() {
-        console.log(`The player ${this.name} has the char ${this.nickname} at level ${this.level}`);
-    }
-}
-const allana = new UserAccount("Allana", 26);
+    Object.defineProperty(CharAccount.prototype, "getLevel", {
+        //accessors (getters e setters)
+        //o get funciona como uma função mas é chamado como um método
+        //tanto o get quanto o set serve para ecmascript 5 ou mais
+        get: function () {
+            return this.level;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(CharAccount.prototype, "setLevel", {
+        set: function (level) {
+            this.level = level;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    CharAccount.prototype.logCharDetails = function () {
+        console.log("The player ".concat(this.name, " with ").concat(this.age, " year(s) old, has the char ").concat(this.nickname, " at level ").concat(this.level));
+    };
+    return CharAccount;
+}(UserAccount));
+var allana = new UserAccount("Allana", 26);
 //usando o nodemon, podemos acessar tanto o objeto inteiro quanto alguma das propriedades desse objeto, assim como seus métodos
 console.log(allana);
-console.log(allana.age);
+//não pode ser chamada pois está como protected
+//console.log(allana.age)
 allana.logDetails();
-const marshmallow = new CharAccount("Marshmallow", 1, "oMaioral", 1000);
+var marshmallow = new CharAccount("Marshmallow", 1, "oMaioral", 1000);
 //não consigo alterar o nickname depois de colocar o "private" pois ele é acessável apenas dentro da classe CharAccount
 //marshmallow.nickname = "marsh"
 console.log(marshmallow);
+//o readonly permite que você leia fora da classe, mas não pode alterar as informações (apenas leitura)
+console.log(marshmallow.level);
 marshmallow.logDetails();
 //mesmo com o private eu consigo acessar o metodo logChar pq eu posso chamar a propriedade dentro da classe com o private, inclusive posso editar, porem nunca fora
 marshmallow.logCharDetails();
+// o get level vc n precisa chamar como se fosse um método, pois ele funciona como uma propriedade qualquer daquele objeto
+console.log(marshmallow.getLevel);
+//o set não funciona igual ao get, pois vc tem que indicar o valor que aquela propriedade tem que ter agora (n funciona como método)
+marshmallow.setLevel = 500;
--- primeiro-programa-com-ts/classes.ts
+++ primeiro-programa-com-ts/classes.ts
@@ -1,8 +1,12 @@
 //classes (poo)
 
+//se eu colocasse que ele era uma classe abstrata, eu poderia apenas extender aquela classe mas não poderia fazer novos objetos a partir dela (interessante quando se quer criar classes que sejam apenas modelos)
+// abstract class UserAccount {
 class UserAccount {
-    name: string;
-    age: number;
+    //o public é implícito, ele indica que pode ser tanto lido quanto editado fora da classe (publico)
+    public name: string;
+    //protected permite que seja chamado dentro da classe ou da classe que estende ela, enquanto o private permite apenas dentro daquela classe
+    protected age: number;
 
     constructor(name: string, age: number) {
         this.name = name;
@@ -28,17 +32,28 @@ class CharAccount extends UserAccount {
 
         this.nickname = nickname;
         this.level = level;
-
     }
+    //accessors (getters e setters)
+    //o get funciona como uma função mas é chamado como um método
+    //tanto o get quanto o set serve para ecmascript 5 ou mais
+    get getLevel() {
+        return this.level
+    }
+
+    set setLevel(level: number) {
+        this.level = level
+    }
+
     logCharDetails(): void {
-        console.log(`The player ${this.name} has the char ${this.nickname} at level ${this.level}`)
+        console.log(`The player ${this.name} with ${this.age} year(s) old, has the char ${this.nickname} at level ${this.level}`)
     }
 }
 
 const allana = new UserAccount("Allana", 26);
 //usando o nodemon, podemos acessar tanto o objeto inteiro quanto alguma das propriedades desse objeto, assim como seus métodos
 console.log(allana)
-console.log(allana.age)
+//não pode ser chamada pois está como protected
+//console.log(allana.age)
 allana.logDetails();
 
 
@@ -46,9 +61,18 @@ const marshmallow = new CharAccount("Marshmallow", 1, "oMaioral", 1000);
 //não consigo alterar o nickname depois de colocar o "private" pois ele é acessável apenas dentro da classe CharAccount
 //marshmallow.nickname = "marsh"
 console.log(marshmallow);
+//o readonly permite que você leia fora da classe, mas não pode alterar as informações (apenas leitura)
+console.log(marshmallow.level);
 marshmallow.logDetails();
 //mesmo com o private eu consigo acessar o metodo logChar pq eu posso chamar a propriedade dentro da classe com o private, inclusive posso editar, porem nunca fora
 marshmallow.logCharDetails();
+
+
+// o get level vc n precisa chamar como se fosse um método, pois ele funciona como uma propriedade qualquer daquele objeto
+console.log(marshmallow.getLevel);
+//o set não funciona igual ao get, pois vc tem que indicar o valor que aquela propriedade tem que ter agora (n funciona como método)
+marshmallow.setLevel = 500;
+
 
 
 
